6a79be89e5a8b1dea4977d67e5b727afc27c7c3f
TangoForce <90145286+TangoForce@users.noreply.github.com>
2022-02-02 16:30:45 +0300

    4-ая работа первый коммит

---
+++ script.js
@@ -0,0 +1,15 @@
+let formElement = document.querySelector('.popup');
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    let nameInput = document.querySelector()
+}
+
+function openForm() {
+	formElement.style.display = 'flex'
+}
+
+function closeForm() {
+	formElement.style.display = 'none'
+}
+
