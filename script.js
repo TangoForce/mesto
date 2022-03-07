adc8d2bccb39c2905160e5c590be6ee50ee728d4
TangoForce <90145286+TangoForce@users.noreply.github.com>
2022-03-07 16:49:58 +0300

    первый коммит

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let formElement = document.querySelector('.popup');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__job');
 
-function formSubmitHandler(event) {
-    event.preventDefault();
-    let nameInput = document.querySelector()
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    let profileName = document.querySelector('.profile__name');
+    let profileDscrp = document.querySelector('.profile__description');
+    profileName = nameInput.value;
+    profileDscrp = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileDscrp.textContent = jobInput.value;
+    formElement.addEventListener('submit', formSubmitHandler);
 }
 
-function openForm() {
-	formElement.style.display = 'flex'
-}
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let saveButton = document.querySelector('.popup__save-button');
 
-function closeForm() {
-	formElement.style.display = 'none'
-}
-
+editButton.addEventListener('click', function () {
+    formElement.classList.toggle('popup_opened');
+});
+closeButton.addEventListener('click', function () {
+    formElement.classList.toggle('popup_opened');
+});
+saveButton.addEventListener('click', function () {
+    formElement.classList.toggle('popup_opened');
+});
